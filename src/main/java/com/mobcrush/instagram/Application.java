177c5def590e22dbbc660d5742806d0318f7c9f9
Maxim Sekerjitsky <msekerjitsky@tula.co>
2018-04-19 15:49:10 +0300

    Add logic in main class.

--- src/main/java/com/mobcrush/instagram/Application.java
+++ src/main/java/com/mobcrush/instagram/Application.java
@@ -1,6 +1,15 @@
 package com.mobcrush.instagram;
 
+import com.mobcrush.instagram.domain.CreateLiveResponse;
+import com.mobcrush.instagram.request.CreateLiveRequest;
+import com.mobcrush.instagram.request.CreateLiveResult;
+import com.mobcrush.instagram.request.StartLiveRequest;
+import com.mobcrush.instagram.request.StartLiveResult;
+import com.mobcrush.instagram.request.payload.CreateLivePayload;
+import com.mobcrush.instagram.request.payload.StartLivePayload;
 import com.mobcrush.instagram.service.AuthenticateService;
+import com.mobcrush.instagram.service.FFmpegRunnerService;
+import com.mobcrush.instagram.service.InstagramBroadcastingService;
 import org.brunocvcunha.instagram4j.Instagram4j;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +22,26 @@ public class Application {
     public static void main(String[] args) {
         try {
             Instagram4j instagram = new AuthenticateService().login(TEST_USERNAME, TEST_PASSWORD);
+
+            CreateLivePayload payload = new CreateLivePayload();
+            payload.set_uuid(instagram.getUuid());
+            payload.set_csrftoken(instagram.getOrFetchCsrf());
+            payload.setPreview_height("1184");
+            payload.setPreview_width("720");
+            payload.setBroadcast_message("");
+            payload.setBroadcast_type("RTMP");
+            payload.setInternal_only("0");
+            CreateLiveRequest request = new CreateLiveRequest(payload);
+            CreateLiveResult response = instagram.sendRequest(request);
+
+            StartLivePayload startLivePayload = new StartLivePayload();
+            startLivePayload.set_uuid(instagram.getUuid());
+            startLivePayload.set_csrftoken(instagram.getOrFetchCsrf());
+            startLivePayload.setShould_send_notifications("1");
+            StartLiveRequest startLiveRequest = new StartLiveRequest(startLivePayload, response.getBroadcastId());
+            StartLiveResult startLiveResult = instagram.sendRequest(startLiveRequest);
+
+//            FFmpegRunnerService.run("", "");
         } catch (Exception ex) {
             LOG.error("Something went wrong: ", ex);
         }
