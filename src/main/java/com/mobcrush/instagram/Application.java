e9cd69fab047f7e4d4060f9d2a2381b3e30e2de1
Maxim Sekerjitsky <msekerjitsky@tula.co>
2018-04-19 18:52:33 +0300

    Add parsing input parameters.

--- src/main/java/com/mobcrush/instagram/Application.java
+++ src/main/java/com/mobcrush/instagram/Application.java
@@ -15,16 +15,25 @@ import org.brunocvcunha.instagram4j.Instagram4j;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.Optional;
 
 
 public class Application {
+
     private static org.slf4j.Logger LOG = LoggerFactory.getLogger(Application.class);
-    private static String TEST_USERNAME = "ivan.mobile";
-    private static String TEST_PASSWORD = "ivan.mobile84";
+
+    private static final String USER_PARAMETER_NAME = "user";
+    private static final String PASSWORD_PARAMETER_NAME = "password";
+    private static final String FILE_PARAMETER_NAME = "file";
 
     public static void main(String[] args) {
+        String user = parseParameter(USER_PARAMETER_NAME, args);
+        String password = parseParameter(PASSWORD_PARAMETER_NAME, args);
+        String videoFile = parseParameter(FILE_PARAMETER_NAME, args);
+
         try {
-            Instagram4j instagram = new AuthenticateService().login(TEST_USERNAME, TEST_PASSWORD);
+            Instagram4j instagram = new AuthenticateService().login(user, password);
 
             CreateLivePayload payload = new CreateLivePayload();
             payload.set_uuid(instagram.getUuid());
@@ -44,14 +53,30 @@ public class Application {
             StartLiveRequest startLiveRequest = new StartLiveRequest(startLivePayload, response.getBroadcastId());
             StartLiveResult startLiveResult = instagram.sendRequest(startLiveRequest);
 
-            URI uri =  new URIBuilder(response.getUploadUrl())
-                    .setScheme("rtmp")
-                    .setPort(80)
-                    .build();
-            FFmpegRunnerService.run("c:\\Downloads\\sample.mp4", uri.toString());
+//            URI uri =  new URIBuilder(response.getUploadUrl())
+//                    .setScheme("rtmp")
+//                    .setPort(80)
+//                    .build();
+            FFmpegRunnerService.run(videoFile, response.getUploadUrl());
         } catch (Exception ex) {
             LOG.error("Something went wrong: ", ex);
         }
 
+    }
+
+    private static String parseParameter(String name, String[] args) {
+        Optional<String> optional = Arrays.stream(args)
+                .filter(arg ->
+                        arg.startsWith("--" + name)
+                )
+                .map(arg -> {
+                    String[] split = arg.split("=");
+                    if (split.length == 2) {
+                        return split[1];
+                    }
+                    throw new IllegalArgumentException("Cannot find value for parameter '" + name + "'");
+                }).findFirst();
+
+        return optional.get();
     }
 }
