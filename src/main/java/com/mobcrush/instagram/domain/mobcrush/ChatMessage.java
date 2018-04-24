b399f872742fe24da41767b6523b065dde9b7c11
Maxim Sekerjitsky <msekerjitsky@tula.co>
2018-04-24 16:24:57 +0300

    Add logic for communication with Mobcrash.

---
+++ src/main/java/com/mobcrush/instagram/domain/mobcrush/ChatMessage.java
@@ -0,0 +1,32 @@
+package com.mobcrush.instagram.domain.mobcrush;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class ChatMessage {
+
+    @JsonProperty("text")
+    private String message;
+    @JsonProperty("contentType")
+    private String contentType = "text/plain";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("message", message)
+                .append("contentType", contentType)
+                .toString();
+    }
+}
